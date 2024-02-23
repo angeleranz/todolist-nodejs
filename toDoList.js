4a009a49ed4ea69662e7f2a7fc2918b799152db2
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-02-22 22:25:25 -0300

    colocando o menu

--- toDoList.js
+++ toDoList.js
@@ -2,64 +2,59 @@
 var readline = require("readline-sync");
 
 let tasks = [];
+let exitProgram = false; // Variável de controle
 
 function start() {
-  let action = readline.question(
-    "Please choose an option to organize your life: \n  1. Add task \n  2. Edit task \n  3. Show to do list \n  4. Remove task \n"
-  );
-  switch (action) {
-    case "1":
-      console.log(addTask());
-      break;
-    case "2":
-      console.log(editTasks());
-      break;
-    case "3":
-      console.log(showTasks());
-      break;
-    case "4":
-      console.log(removeTask());
-      break;
-    default:
-      console.error("Invalid option. Please choose an option between 1 and 4.");
-      break;
+  while (!exitProgram) { // Loop principal
+    let action = readline.question(
+        "Please choose an option to organize your life: \n   1. Add task \n   2. Edit task \n   3. Show to do list \n   4. Remove task \n   5. Find task \n   6. Exit \n"
+    );
+    switch (action) {
+      case "1":
+        addTask();
+        break;
+      case "2":
+        editTask();
+        break;
+      case "3":
+        showTasks(tasks);
+        break;
+      case "4":
+        removeTask();
+        break;
+      case "5":
+        findTask();
+        break;
+      case "6":
+        console.log("Exiting...");
+        exitProgram = true; // Ativa a variável de controle para sair do loop
+        setTimeout(() => { // Adiciona um atraso de 3 segundos antes de sair
+          process.exit(); // Sai do programa após 3 segundos
+        }, 3000);
+        break;
+      default:
+        console.error(
+            "Invalid option. Please choose an option between  1 and  6."
+        );
+        break;
+    }
   }
 }
 
 start();
 
-function backToStart() {
-  start();
-}
-
 function addTask() {
   let newTask = readline.question("Add a task to your list: ");
   tasks.push(newTask);
   console.log("Task " + `"${newTask}"` + " successfully added!");
-  backToStart();
 }
+
 // Código Júlia 01 - Edit task
-
-function editTasks(array, indices, editedTasks) {
-  if (indices.length !== editedTasks.length) {
-    console.log("Invalid ID. Please try again");
-    return array;
-  } else {
-    for (let i = 0; i < indices.length; i++) {
-      array[indices[i]] = editedTasks[i];
-    }
-    return array;
-  }
+function editTask() {
+  // Implemente a função de edição de tarefa aqui
 }
 
-/*
-let tasks = ["Clean my bedroom", "Code tank", "Buy meat"];
-
-console.log(editTasks(tasks, [0, 2], ["Go on dog walk", "Workout"]))
-*/
-
-//código da Julia 02 - Show tasks
-
+// Código da Julia 02 - Show tasks
 function showTasks(tasks) {
   let tasksToShow = tasks.map((task, index) => {
     return {
@@ -71,34 +66,26 @@ function showTasks(tasks) {
   console.table(tasksToShow, ["tasks"]);
 }
 
-/*
-showTasks(tasks);
-*/
-
-//código do Arthur 01 - Remove task
-function removeTask(array, element) {
-  const index = array.indexOf(element);
-  if (index !== -1) {
-    array.splice(index, 1);
+// Função de remoção de tarefa
+function removeTask() {
+  let taskToRemove = readline.question("Enter the index of the task you want to remove: ");
+  taskToRemove = parseInt(taskToRemove);
+  if (isNaN(taskToRemove) || taskToRemove < 0 || taskToRemove >= tasks.length) {
+    console.log("Invalid index. Please enter a valid index.");
+    return;
   }
-  return array;
+  let removedTask = tasks.splice(taskToRemove, 1)[0];
+  console.log(`Task "${removedTask}" successfully removed!`);
 }
-/* exemplo:
-   let tasks = [1,2,3,4];
-   let tasksToRemove = 3;
-   tasks = removeTask(tasks, tasksToRemove);
-   console.log(tasks);
-*/
 
-//código do Arthur 02
-function findTask(array, index) {
-  if (index >= 0 && index < array.length) {
-    console.log(array[index]);
-  } else {
-    console.log("Task index not found.");
+// Função para encontrar uma tarefa
+function findTask() {
+  let indexToFind = readline.question("Enter the index of the task you want to find: ");
+  indexToFind = parseInt(indexToFind);
+  if (isNaN(indexToFind) || indexToFind < 0 || indexToFind >= tasks.length) {
+    console.log("Invalid index. Please enter a valid index.");
+    return;
   }
+  console.log("Task found:");
+  console.log(tasks[indexToFind]);
 }
-/* exemplo:
-let tasks = ["Clean my bedroom", "Code tank", "Buy meat"];
-findTask(tasks, 1);
-*/
